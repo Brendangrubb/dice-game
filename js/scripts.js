d980421066675974a9fbd5436dfc8424205f350c
Brendan Grubb <brendangrubb@gmail.com>
2017-08-18 23:12:16 -0700

    change game format to dice

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
 function getRandomNumber() {
-  var result = Math.floor((Math.random() * 100));
-  return result;
+  var firstDie = Math.floor((Math.random() * 6) + 1);
+  var secondDie = Math.floor((Math.random() * 6) + 1);
+  var computerResult = firstDie + secondDie;
+  var computerArray = [firstDie, secondDie, computerResult];
+  return computerArray;
 }
 
-function userRandomNumber(userInput) {
-  if (userInput === "") {
-    var userResult = Math.floor((Math.random() * 100));
+function userRandomNumber(userInputOne, userInputTwo) {
+  if (userInputOne === "" || userInputTwo === "") {
+    var firstDie = Math.floor((Math.random() * 6) + 1);
+    var secondDie = Math.floor((Math.random() * 6) + 1);
+    var userResult = firstDie + secondDie;
+    var userArray = [firstDie, secondDie, userResult];
   } else {
-    var userResult = parseInt(userInput);
+    var firstDie = parseInt(userInputOne);
+    var secondDie = parseInt(userInputTwo);
+    var userResult = firstDie + secondDie;
+    var userArray = [firstDie, secondDie, userResult];
   }
-  return userResult;
+  return userArray;
 }
 
 function winOrLose(user, computer) {
   if (user > computer) {
     return "You Win!"
+  } else if (user === computer) {
+    return "You're Tied!"
   } else {
     return "You Lose!"
   }
@@ -50,22 +61,23 @@ function winPercentage(userResult, randomNumberResult) {
 $(document).ready(function() {
   $("#engage").click(function() {
     // User Number
-    var userInput = $("#user-number").val();
-    var userResult = userRandomNumber(userInput);
+    var userInputOne = $("#user-number-one").val();
+    var userInputTwo = $("#user-number-two").val();
+    var userResult = userRandomNumber(userInputOne, userInputTwo);
     $("#your-score").empty();
-    $("#your-score").append(userResult)
-    $("#your-score-talley").prepend("<li>" + userResult + "</li>")
+    $("#your-score").append(userResult[0] + " + " + userResult[1] + " = " + userResult[2]);
+    $("#your-score-talley").prepend("<li>" + userResult[2] + "</li>")
     // Computer Number
     var randomNumberResult = getRandomNumber();
     $("#computer-score").empty();
-    $("#computer-score").append(randomNumberResult);
-    $("#computer-score-talley").prepend("<li>" + randomNumberResult + "</li>");
+    $("#computer-score").append(randomNumberResult[0] + " + " + randomNumberResult[1] + " = " + randomNumberResult[2]);
+    $("#computer-score-talley").prepend("<li>" + randomNumberResult[2] + "</li>");
     // Display Result
-    var result = winOrLose(userResult, randomNumberResult);
+    var result = winOrLose(userResult[2], randomNumberResult[2]);
     $("#win-or-lose").empty();
     $("#win-or-lose").append(result);
     $("#win-percentage").empty();
-    $("#win-percentage").append(winPercentage(userResult, randomNumberResult));
+    $("#win-percentage").append(winPercentage(userResult[2], randomNumberResult[2]));
   });
 
 });
