392e4f6ae6e24a18f6feac750762ad390489661a
Brendan Grubb <brendangrubb@gmail.com>
2017-08-18 21:19:22 -0700

    add talley score display

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,14 @@ function winOrLose(user, computer) {
   }
 }
 
+function winPercentage() {
+  var winPercentageMessage =  "computer win total = " +
+                              "C-NUMBER" +
+                              "<br>" +
+                              "user win total = " +
+                              "U-NUMBER";
+  return winPercentageMessage;
+}
 
 $(document).ready(function() {
   $("#engage").click(function() {
@@ -28,14 +36,18 @@ $(document).ready(function() {
     var userResult = userRandomNumber(userInput);
     $("#your-score").empty();
     $("#your-score").append(userResult)
+    $("#your-score-talley").prepend("<li>" + userResult + "</li>")
     // Computer Number
     var randomNumberResult = getRandomNumber();
     $("#computer-score").empty();
     $("#computer-score").append(randomNumberResult);
+    $("#computer-score-talley").prepend("<li>" + randomNumberResult + "</li>");
     // Display Result
     var result = winOrLose(userResult, randomNumberResult);
     $("#win-or-lose").empty();
     $("#win-or-lose").append(result);
+    $("#win-percentage").empty();
+    $("#win-percentage").append(winPercentage());
   });
 
 });
