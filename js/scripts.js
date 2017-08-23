f72e43d745c3e54f8be28478a9e351ba7b0b0488
Brendan Grubb <brendangrubb@gmail.com>
2017-08-23 11:35:24 -0700

    refactor game to change to street craps

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
-function getRandomNumber() {
-  var firstDie = Math.floor((Math.random() * 6) + 1);
-  var secondDie = Math.floor((Math.random() * 6) + 1);
-  var computerResult = firstDie + secondDie;
-  var computerArray = [firstDie, secondDie, computerResult];
-  return computerArray;
+function currentGame(rollCount) {
+  this.rollCount = rollCount;
 }
 
 function userRandomNumber(userInputOne, userInputTwo) {
@@ -21,63 +17,27 @@ function userRandomNumber(userInputOne, userInputTwo) {
   return userArray;
 }
 
-function winOrLose(user, computer) {
-  if (user > computer) {
-    return "You Win!"
-  } else if (user === computer) {
-    return "You're Tied!"
-  } else {
-    return "You Lose!"
-  }
-}
-
-var computerWinTotal = 0;
-var userWinTotal =  0;
-var numberOfWins = 0;
-var numberOfGames = 0;
-function winPercentage(userResult, randomNumberResult) {
-  userWinTotal +=  userResult;
-  computerWinTotal += randomNumberResult;
-  if (userResult > randomNumberResult) {
-    numberOfWins += 1;
-    numberOfGames += 1;
-  } else {
-    numberOfGames += 1;
-  };
-
-  userWinPercentage = Math.floor((numberOfWins/numberOfGames) * 100);
-  var winPercentageMessage =  "computer win total = " +
-                              computerWinTotal +
-                              "<br>" +
-                              "user win total = " +
-                              userWinTotal +
-                              "<br>" +
-                              "user win Percentage = " +
-                              userWinPercentage +
-                              "%";
-  return winPercentageMessage;
-}
 
 $(document).ready(function() {
+  // New Game
+  var newGame = new currentGame(0);
+  $("#new-game").click(function() {
+    newGame = new currentGame(0);
+    $("#engage").removeClass("hide-display");
+    // Empty Fields
+    $("#current-roll").empty();
+    $("#your-roll-talley").empty();
+    $("#result").empty();
+  })
+
+
   $("#engage").click(function() {
-    // User Number
+        // User Number
     var userInputOne = $("#user-number-one").val();
     var userInputTwo = $("#user-number-two").val();
     var userResult = userRandomNumber(userInputOne, userInputTwo);
-    $("#your-score").empty();
-    $("#your-score").append(userResult[0] + " + " + userResult[1] + " = " + userResult[2]);
-    $("#your-score-talley").prepend("<li>" + userResult[2] + "</li>")
-    // Computer Number
-    var randomNumberResult = getRandomNumber();
-    $("#computer-score").empty();
-    $("#computer-score").append(randomNumberResult[0] + " + " + randomNumberResult[1] + " = " + randomNumberResult[2]);
-    $("#computer-score-talley").prepend("<li>" + randomNumberResult[2] + "</li>");
-    // Display Result
-    var result = winOrLose(userResult[2], randomNumberResult[2]);
-    $("#win-or-lose").empty();
-    $("#win-or-lose").append(result);
-    $("#win-percentage").empty();
-    $("#win-percentage").append(winPercentage(userResult[2], randomNumberResult[2]));
+    $("#current-roll").empty();
+    $("#current-roll").append(userResult[0] + " + " + userResult[1] + " = " + userResult[2]);
+    $("#your-roll-talley").prepend("<li>" + userResult[2] + "</li>")
   });
-
 });
