5d41abfee212efaa9766cded5932339f4ce13a0e
Brendan Grubb <brendangrubb@gmail.com>
2017-08-28 17:01:29 -0700

    add player constructor adn display wins, losses and cash

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ function currentGame(rollCount, point) {
   this.point = point;
 }
 
+function currentPlayer() {
+  this.wins = 0;
+  this.losses = 0;
+  this.cash = 100;
+}
+
 // Player Roll Function
 function userRandomNumber(userInputOne, userInputTwo) {
   // if (userInputOne === "" || userInputTwo === "") {
@@ -31,20 +37,24 @@ function getPoint(roll, rollCount) {
   }
   return point;
 }
-function winOrLose(roll, rollCount, point) {
-  console.log(point);
+
+function winOrLose(roll, rollCount, point, player) {
   if (rollCount === 1) {
     if (roll === 7 | roll === 11) {
+      player.wins += 1;
       var result = "You Pass, Motherfuker!";
     } else if (roll === 2 | roll === 3 | roll === 12) {
+      player.losses += 1;
       var result = "Tough Titties, You Crapped Out.";
     } else {
       var result = "Keep going, the <b>point</b> is " + roll;
     }
   } else {
     if (roll === point) {
+      player.wins += 1;
       var result = "You Hit the Point, You Win!!";
     } else if (roll === 7) {
+      player.losses += 1;
       var result = "You Sevened Out!";
     } else {
       var result = "You didn't hit 7 or " + point + ", keep rolling";
@@ -54,8 +64,11 @@ function winOrLose(roll, rollCount, point) {
 }
 
 $(document).ready(function() {
-  // New Game
+  // Game and Player Objects
   var newGame = new currentGame(0);
+  var player = new currentPlayer();
+
+  // New Game
   $("#new-game").click(function() {
     newGame = new currentGame(0);
     $("#engage").removeClass("hide-display");
@@ -69,11 +82,10 @@ $(document).ready(function() {
     $("#point").append("n/a");
   })
 
-
   $("#engage").click(function() {
-    // Game Object
     newGame.rollCount += 1;
     console.log(newGame);
+    console.log(player);
 
     // Gather and Display User Number
     var userInputOne = $("#user-number-one").val();
@@ -89,7 +101,7 @@ $(document).ready(function() {
     }
 
     // Destermine Win, Lose or Continue
-    var result = winOrLose(userResult[2], newGame.rollCount, newGame.point);
+    var result = winOrLose(userResult[2], newGame.rollCount, newGame.point, player);
     if (result === "You Pass, Motherfuker!" |
         result === "Tough Titties, You Crapped Out." |
         result === "You Hit the Point, You Win!!" |
@@ -106,5 +118,14 @@ $(document).ready(function() {
     }
     $("#roll-count").empty();
     $("#roll-count").append(newGame.rollCount);
+    $("#cash").empty();
+    $("#cash").append("$" + player.cash);
+
+    // Player Data Display
+    $("#games-won").empty();
+    $("#games-won").append(player.wins);
+    $("#games-lost").empty();
+    $("#games-lost").append(player.losses);
+
   });
 });
